62a4eed6bd5cd6911a01375ef4cf59dba10e1706
letuanjvb <117722114+letuanjvb@users.noreply.github.com>
2022-11-15 14:17:34 +0700

    Delete Template#2 directory

--- Template#2/darkmode.js
+++
@@ -1,6 +0,0 @@
-let dark_mode_toggle = document.querySelector('.dark-mode-switch')
-
-dark_mode_toggle.onclick = () => {
-    document.querySelector('.wrapper').classList.toggle('light')
-    document.querySelector('.wrapper').classList.toggle('dark')
-}
